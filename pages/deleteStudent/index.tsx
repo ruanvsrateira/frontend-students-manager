7318786136acc342fa1f6ca66143b49208ee5155
ruanvsrateira <101360876+ruanvsrateira@users.noreply.github.com>
2022-04-22 16:23:40 -0300

    fix: bugs  in deply

--- pages/deleteStudent/index.tsx
+++ pages/deleteStudent/index.tsx
@@ -1,52 +1,64 @@
 import React from 'react';
-
-import { useRouter } from 'next/router';
-
-import { styles } from './styles';
-
 import Header from '../../src/components/Header';
+import { useRouter } from 'next/router';
 import { Box, Button, Container, Typography } from '@mui/material';
 import Link from 'next/link';
 import axios from 'axios';
 
 const deleteStudent: React.FC = () => { 
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter();
-    
     const {
-        query: { id, name },
-    } = router
-    
-    const deleteStudent = async(id: number) => {
+        query: { name, id }
+    } = router; 
+
+    const deleteStudent = async() => {
         const data = await axios.get(`http://localhost:3333/students/${id}/delete`);
+        
+        console.log(data);
 
-        window.location.href = "/"
+        router.push("/");
     }
-
 
     return(
         <>
             <Header />
            
             <Container
-                style={styles.mainContainer}
+                style={{ 
+                    justifyContent: "start",    
+                    margin: "100px auto 0px auto" 
+                }}
             >
                 <Typography
-                    style={styles.title}
+                    style={{
+                        fontFamily: "Poppins",
+                        color: "#000",
+                        fontSize: "30px",
+                    }}
                 >
-                    Realmente deseja deletar o aluno { name } ?
+                    Realmente deseja deletar o aluno {name}?
                 </Typography>
                 <Box
                     marginTop="20px"
                 >
                     <Button
-                        style={styles.confirmButton}
-                        onClick={() => deleteStudent(Number(id))}
+                        style={{
+                            backgroundColor: "rgb(0 232 143)",
+                            color: "#fff"
+                        }}
+                        onClick={deleteStudent}
                     >
                         Deletar
                     </Button>
                     <Link href="/" passHref>
                         <Button
-                            style={styles.cancelButton}
+                            style={{
+                                backgroundColor: "#E53E3E",
+                                marginLeft: "10px",
+                                color: "#fff"
+                            }}
                         >
                             Cancelar
                         </Button>
