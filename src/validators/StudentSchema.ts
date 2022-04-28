b4af53599c797cfc976877dace9d10b70e7fa6da
ruanvsrateira <101360876+ruanvsrateira@users.noreply.github.com>
2022-04-28 03:17:32 -0300

    feat: added test and validator of cpf

--- src/validators/StudentSchema.ts
+++ src/validators/StudentSchema.ts
@@ -1,8 +1,10 @@
 import * as yup from 'yup';
+import { validateCPF } from '../utils/validateCPF';
 
 export const StudentSchema = yup.object().shape({
     name: yup.string().required(),
     email: yup.string().required(),
-    cpf: yup.string().required(),
+    cpf: yup.string().required()
+        .test("is-cpf", "CPF inválido", value => value ? validateCPF(value) : true),
     age: yup.number().required()
 })
